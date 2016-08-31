5d6394445ad8aefb82763a76ca7d1e1528d59577
ar.shestopal <ar.shestopal@gmail.com>
2016-08-31 16:00:35 +0300

    is, not methods added

--- developer.rb
+++ developer.rb
@@ -1,27 +1,51 @@
 class Developer
+  attr_accessor :skill_level
   def initialize
+    #default values
     @crazy = true
     @skill_level = :high
     @in = :london
     @love = :ruby
     @want = ['fun', 'money']
-    @positive = true
+    @work = :well
+    define_reader_methods
   end
 
   def crazy
-    return self if @crazy == @positive
+    return self if @crazy
+  end
+
+  #overriding love method to allow syntax like this 'love :ruby, rails'
+  #or not.love :ruby, :rails
+  def love(*args)
+    love = @love.to_sym
+    return self if args.flatten!.include?(love) || args.empty?
   end
 
   # alias for self
-  # kind of syntactic sugar
+  # syntactic sugar
   def are
     self
   end
 
-  # passes args further
-  # kind of syntactic sugar
+  # allows call like 'skill_level.is :high'
   def is(*args)
-    args.flatten!
+    self if equal_to_previous_call(*args)
+  end
+
+  # allows call like 'skill_level.not :low'
+  def not(*args)
+    self unless equal_to_previous_call(*args)
+  end
+
+  #syntactic shugar makes call like 'work well' to run
+  def well
+    :well
+  end
+
+  #syntactic shugar makes call like 'work bad' to run
+  def bad
+    :bad
   end
 
   private
@@ -33,30 +57,26 @@ class Developer
   def define_reader_methods
     method_names = instance_variables.map { |iv| iv.to_s[1..-1] }
     method_names.each do |method_name|
-      singletone_class.send(:define_method, method_name) do |*args|
-        args.flatten!
-        return self if args.empty?
-        reader_method_body(arsg, method_name)
+      self.class.send(:define_method, method_name) do |*args|
+        reader_method_body(args, method_name)
       end
     end
   end
 
+  #reset @positive after .not method call
   def reader_method_body(args, method_name)
+    args.flatten!
     args = args[0] if args.size == 1
-    if attr_equal(args) || attr_not_equal(args)
+    instance_exec do
+      value = instance_variable_get("@#{method_name}")
+      @prev_result = value if value == args || args.empty?
       self
-    else
-      nil
     end
-  rescue => NoMethodError
-    nil
   end
 
-  def attr_equal(args)
-    self.send("@#{method_name}") == args && @positive
-  end
-
-  def attr_not_equal(args)
-    self.send("@#{method_name}") != args && !@positive
+  def equal_to_previous_call(*args)
+    args.flatten
+    args = args.first if args.size == 1
+    @prev_result == args
   end
 end
--- spec/developer_spec.rb
+++ spec/developer_spec.rb
@@ -3,17 +3,52 @@ require 'spec_helper'
 describe Developer do
   it { should respond_to(:are) }
   it { should respond_to(:crazy) }
-  it { should respond_to(:crazy=) }
+  it { should respond_to(:in) }
+  it { should respond_to(:love) }
+  it { should respond_to(:skill_level) }
+  it { should respond_to(:want) }
 
-  describe 'select' do
+  # it { should respond_to(:is) }
+  # it { should respond_to(:not) }
+
+  describe 'methods' do
     before(:each) do
       @developer = Developer.new
       @developers = [@developer]
     end
 
-    it 'should select crazy developer by default' do
-      selected = @developers.select { |you| you.are.crazy }
-      expect(selected).to eq([@developer])
+    context 'reader_methods' do
+      it 'should select crazy developer by default' do
+        selected = @developers.select { |you| you.are.crazy }
+        expect(selected).to eq([@developer])
+      end
+
+      it 'shold select high level developer by default' do
+        selected = @developers.select { |you| you.skill_level :high }
+        expect(selected).to eq([@developer])
+      end
+    end
+
+    context 'not' do
+      it 'shuld return falsy result with not' do
+        expect(@developer.skill_level.not :high).to be_falsy
+      end
+    end
+
+    context 'is' do
+      it 'should return args' do
+        expect(@developer.skill_level.is :high).to be_truthy
+      end
+    end
+
+    context 'chaining methods for select' do
+      it 'allows chaing methods' do
+        expect(@developer.love.skill_level).to be_truthy
+      end
+
+      it 'allows chaining methods in any order' do
+        expect(@developer.skill_level.love).to be_truthy
+      end
     end
   end
 end
